82ef693d6a291a0402363a0c43088359fd39f30c
Ruan Carmo Silva <ruancarmosilva32@gmail.com>
2026-03-11 22:24:27 -0300

    Primeiro projeto: calculadora em C#

---
+++ CalculadoraRCS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraRCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Escolha uma das opções: (+, -, *, /) ");
+            string opções = Console.ReadLine();
+
+            double resultado = 0;
+
+            switch (opções)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    return;
+
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+
+            Console.ReadLine();
+
+        }
+    }
+}
